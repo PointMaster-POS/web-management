6764465fc8d7a11d0084a4954dee8ad1276d1061
Dil0518 <dilrangi20sankalpana@gmail.com>
2024-07-07 19:59:42 +0530

    Update App.js

--- pointmaster/src/App.js
+++ pointmaster/src/App.js
@@ -1,17 +1,37 @@
+import ReactDOM from "react-dom";
+import Dashboard from "./Component/Owner's Dashboard/Dashboard";
+import Stores from "./Component/Owner's Dashboard/Stores";
+import Users from "./Component/Owner's Dashboard/Users";
+import Suppliers from "./Component/Owner's Dashboard/Suppliers";
+import Category from "./Component/Owner's Dashboard/Category";
+import Products from "./Component/Owner's Dashboard/Products";
+import Orders from "./Component/Owner's Dashboard/Orders";
+import Reports from "./Component/Owner's Dashboard/Reports";
+import Expired from "./Component/Owner's Dashboard/Expired";
+import { BrowserRouter,Routes,Route } from "react-router-dom";
+// import LogIn from "./Component/Login/LogIn";
 
-
-import  Customers from './pages/CustomerPages/Customers';
-import CustomerInfo from './pages/CustomerPages/CustomerInfo';
 
 function App() {
-  //comment from Himindu
-  //Commment from Pavani Karunarathna
   return (
-    <div className="App">
-      <Customers/>
+    <div>
+      {/* <LogIn /> */}
+      {<BrowserRouter>
+        <Routes>
+        <Route path="/" element={<Dashboard />} />
+          <Route path="/stores" element={<Stores />} />
+          <Route path="/users" element={<Users />} />
+          <Route path="/suppliers" element={<Suppliers />} />
+          <Route path="/category" element={<Category />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/orders" element={<Orders />} />
+          <Route path="/reports" element={<Reports />} />
+          <Route path="/expired" element={<Expired />} />
+          <Route path="/logout" element={<div>Logout Page</div>} />
+        </Routes>
+      </BrowserRouter> }
     </div>
   );
 }
-
 
 export default App;
