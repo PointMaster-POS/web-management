e44e49e4aae18c63c35d3c2365783290fe7a1fe1
Dil0518 <dilrangi20sankalpana@gmail.com>
2024-07-08 01:16:57 +0530

    Update App.js

--- pointmaster/src/App.js
+++ pointmaster/src/App.js
@@ -1,4 +1,6 @@
 import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MainLayout from "./Component/Owner's Dashboard/MainLayout";
 import Dashboard from "./Component/Owner's Dashboard/Dashboard";
 import Stores from "./Component/Owner's Dashboard/Stores";
 import Users from "./Component/Owner's Dashboard/Users";
@@ -8,30 +10,27 @@ import Products from "./Component/Owner's Dashboard/Products";
 import Orders from "./Component/Owner's Dashboard/Orders";
 import Reports from "./Component/Owner's Dashboard/Reports";
 import Expired from "./Component/Owner's Dashboard/Expired";
-import { BrowserRouter,Routes,Route } from "react-router-dom";
-// import LogIn from "./Component/Login/LogIn";
-
 
 function App() {
   return (
-    <div>
-      {/* <LogIn /> */}
-      {<BrowserRouter>
-        <Routes>
-        <Route path="/" element={<Dashboard />} />
-          <Route path="/stores" element={<Stores />} />
-          <Route path="/users" element={<Users />} />
-          <Route path="/suppliers" element={<Suppliers />} />
-          <Route path="/category" element={<Category />} />
-          <Route path="/products" element={<Products />} />
-          <Route path="/orders" element={<Orders />} />
-          <Route path="/reports" element={<Reports />} />
-          <Route path="/expired" element={<Expired />} />
-          <Route path="/logout" element={<div>Logout Page</div>} />
-        </Routes>
-      </BrowserRouter> }
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="stores" element={<Stores />} />
+          <Route path="users" element={<Users />} />
+          <Route path="suppliers" element={<Suppliers />} />
+          <Route path="category" element={<Category />} />
+          <Route path="products" element={<Products />} />
+          <Route path="orders" element={<Orders />} />
+          <Route path="reports" element={<Reports />} />
+          <Route path="expired" element={<Expired />} />
+          <Route path="logout" element={<div>Logout Page</div>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
+ReactDOM.render(<App />, document.getElementById("root"));
 export default App;
