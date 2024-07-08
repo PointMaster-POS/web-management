68310312c8121b524e3bdcd81ff7cc0ef398c156
PavaniKarunarathna <pavanikarunarathna21113@gmail.com>
2024-07-08 17:16:32 +0530

    Adding suppliers details list page

--- pointmaster/src/App.js
+++ pointmaster/src/App.js
@@ -4,13 +4,14 @@ import  Customers from './pages/CustomerPages/Customers';
 import CustomerInfo from './pages/CustomerPages/CustomerInfo';
 import InventoryDashboard from './pages/Inventory Pages/InventoryDashboard';
 import Category from './pages/Inventory Pages/Category';
+import Suppliers from './pages/Inventory Pages/Suppliers';
 
 function App() {
   //comment from Himindu
   //Commment from Pavani Karunarathna
   return (
     <div className="App">
-      <Category/>
+      <Suppliers/>
     </div>
   );
 }
