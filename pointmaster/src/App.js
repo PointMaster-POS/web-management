2f5ac5349eee4962419e6238f841859bd23d2925
Himindu Kularathne <143618014+Himindu-Kularathne@users.noreply.github.com>
2024-07-10 10:07:33 +0530

    make context folder and initate the intro slide view

--- pointmaster/src/App.js
+++ pointmaster/src/App.js
@@ -3,13 +3,13 @@
 import  Customers from './pages/CustomerPages/Customers';
 import CustomerInfo from './pages/CustomerPages/CustomerInfo';
 import LoyalityMenu from './pages/LoyalityPrograms/LoyalityMenu'
-
+import LoyalityIntro from './pages/LoyalityPrograms/LoyalityIntro';
 function App() {
   //comment from Himindu
   //Commment from Pavani Karunarathna
   return (
     <div className="App">
-      <LoyalityMenu />
+      <LoyalityIntro />
     </div>
   );
 }
--- pointmaster/src/pages/LoyalityPrograms/LoyalityIntro.js
+++ pointmaster/src/pages/LoyalityPrograms/LoyalityIntro.js
@@ -1,13 +1,47 @@
-import "./LoyalityIntro.css";
+import React, { useEffect, useState } from 'react';
+import { Button, Drawer, theme } from 'antd';
+const LoyalityIntro = (props) => {
+  const { token } = theme.useToken();
+  const [open, setOpen] = useState(false);
 
-const LoyalityIntro = () => {
-
-    return (
-        <div>
-            <h1>Loyality Programs</h1>
-            <p>Here you can create and manage your loyality programs.</p>
-        </div>
-    )
-}
-
+  useEffect(() => {
+    setOpen(props.open);
+  }, [props.open]);
+  
+  const onClose = () => {
+    setOpen(false);
+  };
+  const containerStyle = {
+    position: 'relative',
+    height: 200,
+    padding: 48,
+    overflow: 'hidden',
+    background: token.colorFillAlter,
+    border: `1px solid ${token.colorBorderSecondary}`,
+    borderRadius: token.borderRadiusLG,
+  };
+  return (
+    <div style={containerStyle}>
+      Render in this
+      <div
+        style={{
+          marginTop: 16,
+        }}
+      >
+       
+      </div>
+      <Drawer
+        title="Basic Drawer"
+        placement="top"
+        closable={false}
+        onClose={onClose}
+        open={open}
+        getContainer={false}
+        size='large'
+      >
+        <p>Some contents...</p>
+      </Drawer>
+    </div>
+  );
+};
 export default LoyalityIntro;
