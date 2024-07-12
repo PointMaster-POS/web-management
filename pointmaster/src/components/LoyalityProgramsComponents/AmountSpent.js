79749e2eee114090471b473c10f71c20be859b87
Himindu Kularathne <143618014+Himindu-Kularathne@users.noreply.github.com>
2024-07-12 08:31:24 +0530

    created Amount spent all forms

--- pointmaster/src/components/LoyalityProgramsComponents/AmountSpent.js
+++ pointmaster/src/components/LoyalityProgramsComponents/AmountSpent.js
@@ -1,302 +1,64 @@
 import React, { useState } from "react";
-import {
-  Button,
-  message,
-  Steps,
-  theme,
-  Form,
-  Input,
-  Divider,
-    InputNumber,
- Cascader,
-  DatePicker,
-} from "antd";
-
-const { SHOW_CHILD } = Cascader;
-const options = [
-  {
-    label: 'Light',
-    value: 'light',
-    children: new Array(20).fill(null).map((_, index) => ({
-      label: `Number ${index}`,
-      value: index,
-    })),
-  },
-  {
-    label: 'Bamboo',
-    value: 'bamboo',
-    children: [
-      {
-        label: 'Little',
-        value: 'little',
-        children: [
-          {
-            label: 'Toy Fish',
-            value: 'fish',
-          },
-          {
-            label: 'Toy Cards',
-            value: 'cards',
-          },
-          {
-            label: 'Toy Bird',
-            value: 'bird',
-          },
-        ],
-      },
-    ],
-  },
-];
+import { Button, message, Steps, theme, Form } from "antd";
+import AmountSpentFirstForm from './AmountSpentFirstForm';
+import AmountSpentSecondForm from './AmountSpentSecondForm';
+import AmountSpentThirdForm from './AmountSpentThirdForm';
 
 const AmountSpent = () => {
   const { token } = theme.useToken();
   const [current, setCurrent] = useState(0);
-
-
-  const FirstForm = () => {
-    const { RangePicker } = DatePicker;
-    
-
-    const formItemLayout = {
-      labelCol: {
-        xs: {
-          span: 24,
-        },
-        sm: {
-          span: 6,
-        },
-      },
-      wrapperCol: {
-        xs: {
-          span: 24,
-        },
-        sm: {
-          span: 14,
-        },
-      },
-    };
-    return (
-      <div
-        style={{
-          padding: "20px",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Form
-          {...formItemLayout}
-          variant="filled"
-          style={{
-            maxWidth: 600,
-            margin: "auto",
-          }}
-        >
-          <Form.Item
-            label="Program Name"
-            name="Input"
-            rules={[
-              {
-                required: true,
-                message: "Please input!",
-              },
-            ]}
-          >
-            <Input />
-          </Form.Item>
-
-          <Form.Item
-            label="Description"
-            name="TextArea"
-            rules={[
-              {
-                required: true,
-                message: "Please input!",
-              },
-            ]}
-          >
-            <Input.TextArea
-            
-             />
-          </Form.Item>
-
-          <Form.Item
-            label="Select Valid Period"
-            name="RangePicker"
-            rules={[
-              {
-                required: true,
-                message: "Please input!",
-              },
-            ]}
-          >
-            <RangePicker
-              
-
-             />
-          </Form.Item>
-        </Form>
-      </div>
-    );
-  };
-
-  const SecondForm = () => {
-    const { RangePicker } = DatePicker;
-    const formItemLayout = {
-      labelCol: {
-        xs: {
-          span: 24,
-        },
-        sm: {
-          span: 6,
-        },
-      },
-      wrapperCol: {
-        xs: {
-          span: 24,
-        },
-        sm: {
-          span: 14,
-        },
-      },
-    }
-    return (
-        <div
-        style={{
-          padding: "20px",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Form
-          {...formItemLayout}
-          variant="filled"
-          style={{
-            maxWidth: 600,
-            margin: "auto",
-          }}
-        >
-          <Divider orientation="left" orientationMargin="0">
-            Rewards Rate Setup
-    </Divider>
-
-    <Form.Item
-            label="Per Money Spent"
-            name="Per Money Spent"
-            rules={[
-              {
-                required: true,
-                message: "Please enter the ",
-              },
-            ]}
-          >
-            <InputNumber min={1} max={10000} defaultValue={100} />
-          </Form.Item>
-          <Form.Item
-            label="Rewarded Points"
-            name="Points"
-            rules={[
-              {
-                required: true,
-                message: "Please enter the ",
-              },
-            ]}
-          >
-            <InputNumber min={1} max={10} defaultValue={1} />
-          </Form.Item>
-          <Divider orientation="left" orientationMargin="0">
-            Select Valid Period
-    </Divider>
-
-            <Form.Item 
-            label="Period in Months"
-            name="Period"
-            rules={[
-                {
-                    required: true,
-                    message: "Please input!",
-                },
-            ]}
-            
-            >
-
-<InputNumber min={1} max={24} defaultValue={3} />
-          </Form.Item>
-          <Divider orientation="left" orientationMargin="0">
-            Minimum Number of Points to Redeem
-    </Divider>
-
-            <Form.Item 
-            label="Minimum Points"
-            name="Minimum Points"
-            rules={[
-                {
-                    required: true,
-                    message: "Please input!",
-                },
-            ]}
-            >
-
-<InputNumber  defaultValue={100} />
-          </Form.Item>
-        </Form>
-        
-        
-      </div>
-    );
-  };
-
-  const ThirdForm = () => {
-    const onChange = (value) => {
-        console.log(value);
-      };
-    return (
-      <>
-        <Cascader
-        style={{
-          width: '100%',
-        }}
-        options={options}
-        onChange={onChange}
-        multiple
-        maxTagCount="responsive"
-        showCheckedStrategy={SHOW_CHILD}
-        defaultValue={[
-          ['bamboo', 'little', 'fish'],
-          ['bamboo', 'little', 'cards'],
-          ['bamboo', 'little', 'bird'],
-        ]}
-      />
-        </>
-    );
-  };
+  const [form1] = Form.useForm();
+  const [form2] = Form.useForm();
+  const [form3] = Form.useForm();
 
   const steps = [
     {
       title: "Program Details Form",
-      content: <FirstForm />,
+      content: <AmountSpentFirstForm form={form1} />,
     },
     {
       title: "Rewards Setup Form",
-      content: <SecondForm />,
+      content: <AmountSpentSecondForm form={form2} />,
     },
     {
-      title: "Rewaded Items Form",
-      content: <ThirdForm />,
+      title: "Rewarded Items Form",
+      content: <AmountSpentThirdForm form={form3} />,
     },
   ];
+
   const next = () => {
-    
-   
-    setCurrent(current + 1);
+    if (current === 0) {
+      form1.validateFields().then((values) => {
+        console.log('Form 1 Values:', values?  values : "empty");
+        setCurrent(current + 1);
+      });
+    } else if (current === 1) {
+      form2.validateFields().then((values) => {
+        console.log('Form 2 Values:', values);
+        setCurrent(current + 1);
+      });
+    } else {
+      form3.validateFields().then((values) => {
+        console.log('Form 3 Values:', values);
+        setCurrent(current + 1);
+      });
+    }
   };
 
   const prev = () => {
     setCurrent(current - 1);
   };
+
+  const handledDone = () => {
+    message.success("Processing complete!");
+    setCurrent(0);
+  };
+
   const items = steps.map((item) => ({
     key: item.title,
     title: item.title,
   }));
+
   const contentStyle = {
     lineHeight: "260px",
     textAlign: "center",
@@ -307,21 +69,13 @@ const AmountSpent = () => {
     marginTop: 16,
   };
 
-  const handledDone = () => {
-    message.success("Processing complete!");
-    setCurrent(0);
-  };
   return (
     <Form.Provider>
       <Steps current={current} items={items} />
       <div style={contentStyle}>{steps[current].content}</div>
-      <div
-        style={{
-          marginTop: 24,
-        }}
-      >
+      <div style={{ marginTop: 24 }}>
         {current < steps.length - 1 && (
-          <Button type="primary" onClick={() => next()}>
+          <Button type="primary" onClick={next}>
             Next
           </Button>
         )}
@@ -331,12 +85,7 @@ const AmountSpent = () => {
           </Button>
         )}
         {current > 0 && (
-          <Button
-            style={{
-              margin: "0 8px",
-            }}
-            onClick={() => prev()}
-          >
+          <Button style={{ margin: "0 8px" }} onClick={prev}>
             Previous
           </Button>
         )}
--- pointmaster/src/components/LoyalityProgramsComponents/CashForPurchase.js
+++ pointmaster/src/components/LoyalityProgramsComponents/CashForPurchase.js
@@ -1,13 +1,336 @@
+import React, { useState } from "react";
+import {
+  Button,
+  message,
+  Steps,
+  theme,
+  Form,
+  Input,
+  Divider,
+    InputNumber,
+ Cascader,
+  DatePicker,
+} from "antd";
 
+const { SHOW_CHILD } = Cascader;
+const options = [
+  {
+    label: 'Light',
+    value: 'light',
+    children: new Array(20).fill(null).map((_, index) => ({
+      label: `Number ${index}`,
+      value: index,
+    })),
+  },
+  {
+    label: 'Bamboo',
+    value: 'bamboo',
+    children: [
+      {
+        label: 'Little',
+        value: 'little',
+        children: [
+          {
+            label: 'Toy Fish',
+            value: 'fish',
+          },
+          {
+            label: 'Toy Cards',
+            value: 'cards',
+          },
+          {
+            label: 'Toy Bird',
+            value: 'bird',
+          },
+        ],
+      },
+    ],
+  },
+];
 
 const CashForPurchase = () => {
-    return (
-        <div>
-            <h1>Cash for Purchase</h1>
-            <p>Get 1 point for every $1 you spend</p>
-        </div>
-    );
+  const { token } = theme.useToken();
+  const [current, setCurrent] = useState(0);
 
-}
+
+  const FirstForm = () => {
+    const { RangePicker } = DatePicker;
+    
+
+    const formItemLayout = {
+      labelCol: {
+        xs: {
+          span: 24,
+        },
+        sm: {
+          span: 6,
+        },
+      },
+      wrapperCol: {
+        xs: {
+          span: 24,
+        },
+        sm: {
+          span: 14,
+        },
+      },
+    };
+    return (
+      <div
+        style={{
+          padding: "20px",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Form
+          {...formItemLayout}
+          variant="filled"
+          style={{
+            maxWidth: 600,
+            margin: "auto",
+          }}
+        >
+          <Form.Item
+            label="Program Name"
+            name="Input"
+            rules={[
+              {
+                required: true,
+                message: "Please input!",
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            label="Description"
+            name="TextArea"
+            rules={[
+              {
+                required: true,
+                message: "Please input!",
+              },
+            ]}
+          >
+            <Input.TextArea
+            
+             />
+          </Form.Item>
+
+          <Form.Item
+            label="Select Valid Period"
+            name="RangePicker"
+            rules={[
+              {
+                required: true,
+                message: "Please input!",
+              },
+            ]}
+          >
+            <RangePicker
+              
+
+             />
+          </Form.Item>
+        </Form>
+      </div>
+    );
+  };
+
+  const SecondForm = () => {
+    const { RangePicker } = DatePicker;
+    const formItemLayout = {
+      labelCol: {
+        xs: {
+          span: 24,
+        },
+        sm: {
+          span: 6,
+        },
+      },
+      wrapperCol: {
+        xs: {
+          span: 24,
+        },
+        sm: {
+          span: 14,
+        },
+      },
+    }
+    return (
+        <div
+        style={{
+          padding: "20px",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Form
+          {...formItemLayout}
+          variant="filled"
+          style={{
+            maxWidth: 600,
+            margin: "auto",
+          }}
+        >
+          <Divider orientation="left" orientationMargin="0">
+            Rewards Rate Setup
+    </Divider>
+
+          <Form.Item
+            label="Points per Visit"
+            name="Points"
+            rules={[
+              {
+                required: true,
+                message: "Please enter the ",
+              },
+            ]}
+          >
+            <InputNumber min={1} max={10} defaultValue={4} />
+          </Form.Item>
+          <Divider orientation="left" orientationMargin="0">
+            Select Valid Period
+    </Divider>
+
+            <Form.Item 
+            label="Period in Months"
+            name="Period"
+            rules={[
+                {
+                    required: true,
+                    message: "Please input!",
+                },
+            ]}
+            
+            >
+
+<InputNumber min={1} max={24} defaultValue={3} />
+          </Form.Item>
+          <Divider orientation="left" orientationMargin="0">
+            Minimum Number of Points to Redeem
+    </Divider>
+
+            <Form.Item 
+            label="Minimum Points"
+            name="Minimum Points"
+            rules={[
+                {
+                    required: true,
+                    message: "Please input!",
+                },
+            ]}
+            >
+
+<InputNumber  defaultValue={100} />
+          </Form.Item>
+        </Form>
+        
+        
+      </div>
+    );
+  };
+
+  const ThirdForm = () => {
+    const onChange = (value) => {
+        console.log(value);
+      };
+    return (
+      <>
+        <Cascader
+        style={{
+          width: '100%',
+        }}
+        options={options}
+        onChange={onChange}
+        multiple
+        maxTagCount="responsive"
+        showCheckedStrategy={SHOW_CHILD}
+        defaultValue={[
+          ['bamboo', 'little', 'fish'],
+          ['bamboo', 'little', 'cards'],
+          ['bamboo', 'little', 'bird'],
+        ]}
+      />
+        </>
+    );
+  };
+
+  const steps = [
+    {
+      title: "Program Details Form",
+      content: <FirstForm />,
+    },
+    {
+      title: "Rewards Setup Form",
+      content: <SecondForm />,
+    },
+    {
+      title: "Rewaded Items Form",
+      content: <ThirdForm />,
+    },
+  ];
+  const next = () => {
+    
+   
+    setCurrent(current + 1);
+  };
+
+  const prev = () => {
+    setCurrent(current - 1);
+  };
+  const items = steps.map((item) => ({
+    key: item.title,
+    title: item.title,
+  }));
+  const contentStyle = {
+    lineHeight: "260px",
+    textAlign: "center",
+    color: token.colorTextTertiary,
+    backgroundColor: token.colorFillAlter,
+    borderRadius: token.borderRadiusLG,
+    border: `1px dashed ${token.colorBorder}`,
+    marginTop: 16,
+  };
+
+  const handledDone = () => {
+    message.success("Processing complete!");
+    setCurrent(0);
+  };
+  return (
+    <Form.Provider>
+      <Steps current={current} items={items} />
+      <div style={contentStyle}>{steps[current].content}</div>
+      <div
+        style={{
+          marginTop: 24,
+        }}
+      >
+        {current < steps.length - 1 && (
+          <Button type="primary" onClick={() => next()}>
+            Next
+          </Button>
+        )}
+        {current === steps.length - 1 && (
+          <Button type="primary" onClick={handledDone}>
+            Done
+          </Button>
+        )}
+        {current > 0 && (
+          <Button
+            style={{
+              margin: "0 8px",
+            }}
+            onClick={() => prev()}
+          >
+            Previous
+          </Button>
+        )}
+      </div>
+    </Form.Provider>
+  );
+};
 
 export default CashForPurchase;
