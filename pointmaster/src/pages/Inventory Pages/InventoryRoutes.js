ba022415339145c7637619194769a2745fcc8b9a
PavaniKarunarathna <pavanikarunarathna21113@gmail.com>
2024-07-13 00:00:05 +0530

    Update InventoryRoutes.js

--- pointmaster/src/pages/Inventory Pages/InventoryRoutes.js
+++ pointmaster/src/pages/Inventory Pages/InventoryRoutes.js
@@ -6,18 +6,22 @@ import PurchaseHistory from './PurchaseHistory';
 import AddProduct from './AddProduct';
 import ProductList from './ProductList';
 import MainLayout from '../../components/Inventory Components/MainLayout';
+import Profile from './Profile';
+import Settings from './Settings';
 
 function InventoryRoutes() {
   return ( 
     <Routes>
         <Route path="/" element={<MainLayout />}>
-          <Route path="/" element={<InventoryDashboard />} />
-          <Route path="/category" element={<Category />} />
-          <Route path="/suppliers" element={<Suppliers />} />
-          <Route path="/phistory/:supplier_id" element={<PurchaseHistory />} />
-          <Route path="/addproduct" element={<AddProduct />} />
-          <Route path="/productlist" element={<ProductList />}/>
-        </Route>  
+        <Route path="/" element={<InventoryDashboard />} />
+        <Route path="/category" element={<Category />} />
+        <Route path="/suppliers" element={<Suppliers />} />
+        <Route path="/phistory/:supplier_id" element={<PurchaseHistory />} />
+        <Route path="/addproduct" element={<AddProduct />} />
+        <Route path="/productlist" element={<ProductList />}/>
+        <Route path="/profile" element={<Profile/>} />
+        <Route path="/settings" element={<Settings/>} />
+      </Route>  
     </Routes>
   );
 }
