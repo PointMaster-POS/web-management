d09466ee60bebdd8a3a36cc6822b24480be58ed8
Dil0518 <dilrangi20sankalpana@gmail.com>
2024-07-15 22:46:31 +0530

    Update Content.js

--- pointmaster/src/components/Owner's Dashboard/Content.js
+++ pointmaster/src/components/Owner's Dashboard/Content.js
@@ -1,9 +1,15 @@
-import React from 'react'
-import AppRoutes from "./AppRoutes"
+import React from 'react';
+import AppRoutes from "./AppRoutes";
+import { Layout } from 'antd';
+
+const { Content: AntContent } = Layout;
 
 const Content = () => {
   return (
-    <div className='content'><AppRoutes /></div>
+    // <div className='content'><AppRoutes /></div>
+    <AntContent  style={{ paddingTop: 20, minHeight: 360 }}  >
+      <AppRoutes />
+    </AntContent>
   )
 }
 
