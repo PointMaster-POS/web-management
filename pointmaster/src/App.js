04b7aa6b00924414771274a85409eae73c02c5a9
Himindu Kularathne <143618014+Himindu-Kularathne@users.noreply.github.com>
2024-07-18 10:19:33 +0530

    removed unwanted comments

--- pointmaster/src/App.js
+++ pointmaster/src/App.js
@@ -5,8 +5,7 @@ import CustomerInfo from './pages/CustomerPages/CustomerInfo';
 import LoyalityMenu from './pages/LoyalityPrograms/LoyalityMenu'
 import LoyalityIntro from './pages/LoyalityPrograms/LoyalityIntro';
 function App() {
-  //comment from Himindu
-  //Commment from Pavani Karunarathna
+  
   return (
     <div className="App">
       <LoyalityMenu />
