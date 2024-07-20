26aa5d5c596316d89393ab6796ad8f7bee0fd80c
Dil0518 <dilrangi20sankalpana@gmail.com>
2024-07-20 22:17:47 +0530

    Update Content.js

--- pointmaster/src/components/Dashboard/Content.js
+++ pointmaster/src/components/Dashboard/Content.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import OwnerRoutes from '../../pages/Owner Dashboard/OwnerRoutes';
 import InventoryRoutes from '../../pages/Inventory Dashboard/InventoryRoutes';
+import { Layout } from 'antd';
 
+const { Content: AntContent } = Layout;
 
 const Content = () => {
   return (
-    <div className='content'>
-      {/* <InventoryRoutes /> */}
-      <OwnerRoutes />
-    </div>
-  )
+    <div className='content'><OwnerRoutes /></div>
+    // <AntContent  style={{ minHeight: 360 }}  >
+    //   <OwnerRoutes />
+    // </AntContent>
+  )  
 }
 
 export default Content;
