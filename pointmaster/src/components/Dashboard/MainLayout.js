62a9a0c37536ffd6d44065ebc64c3fcb2d9773db
Dil0518 <dilrangi20sankalpana@gmail.com>
2024-07-20 22:17:59 +0530

    Update MainLayout.js

--- pointmaster/src/components/Dashboard/MainLayout.js
+++ pointmaster/src/components/Dashboard/MainLayout.js
@@ -1,25 +1,46 @@
-import React from 'react';
-import {Layout } from 'antd';
-import SideBar from './SideBar';
-import Header from './Header';
-import Content from "./Content"
+import React, { useState } from "react";
+import { Layout } from "antd";
+import SideBar from "./SideBar";
+import Header from "./Header";
+import Content from "./Content";
 // import Footer from "../Footer/Footer";
 
 const { Content: AntContent } = Layout;
 
 const MainLayout = () => {
+  const [collapsed, setCollapsed] = useState(false);
+
+  const handleCollapse = (collapsed) => {
+    setCollapsed(collapsed);
+  };
 
   return (
-    <Layout style={{minHeight: '100vh',}}>
-      <SideBar />
-      <Layout>
+    <Layout style={{ minHeight: "100vh" }}>
+      <div
+        style={{
+          position: "sticky",
+          top: 0,
+          zIndex: 1,
+          width: "100%",
+        }}
+      >
         <Header />
-        <AntContent style={{ margin: '0 16px' }}>
-          <Content />
-        </AntContent>
-        {/* <Footer /> */}
+      </div>
+      <Layout>
+        <SideBar onCollapse={handleCollapse} />
+        <Layout
+          style={{
+            marginLeft: collapsed ? "80px" : "260px", // Adjust based on collapsed state
+            transition: "margin-left 0.2s",
+          }}
+        >
+          <AntContent style={{ margin: "16px 16px 0" }}>
+            <Content />
+          </AntContent>
+        </Layout>
       </Layout>
     </Layout>
   );
 };
+
 export default MainLayout;
