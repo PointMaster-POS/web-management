c166a5d0c07bb210206026d6d66eb2d38d1b673b
Dil0518 <dilrangi20sankalpana@gmail.com>
2024-08-17 20:02:03 +0530

    Update Content.js

--- pointmaster/src/components/Dashboard/Content.js
+++ pointmaster/src/components/Dashboard/Content.js
@@ -7,7 +7,7 @@ const { Content: AntContent } = Layout;
 
 const Content = () => {
   return (
-    <div className='content'>
+    <div >
       {/* <InventoryRoutes /> */}
       <OwnerRoutes />
     </div>
