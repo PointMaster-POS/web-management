3f353cdb0310fd2eba0494817fdea62564ac4c18
PavaniKarunarathna <pavanikarunarathna21113@gmail.com>
2024-08-24 16:39:12 +0530

    landing pages

---
+++ pointmaster/src/pages/Registration/Landing page/Component3.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Component3() {
+  return (
+    <div>
+      <img src={process.env.PUBLIC_URL + '/Images/2.png'} 
+      alt="Description of the image" />
+    </div>
+  );
+}
