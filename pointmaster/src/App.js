c109a62e9eb241d6ae9fc0bad67d6d7709bdaec0
Dil0518 <dilrangi20sankalpana@gmail.com>
2024-09-15 19:10:05 +0530

    Update App.js

--- pointmaster/src/App.js
+++ pointmaster/src/App.js
@@ -1,10 +1,6 @@
 import ReactDOM from "react-dom";
 import MainLayout from "./components/Dashboard/MainLayout";
 import { BrowserRouter } from "react-router-dom";
-import Customers from "./pages/CustomerPages/Customers";
-import CustomerInfo from "./pages/CustomerPages/CustomerInfo";
-import LoyalityMenu from "./pages/LoyalityPrograms/LoyalityMenu";
-import LoyalityIntro from "./pages/LoyalityPrograms/LoyalityIntro";
 import LogIn from "./components/LogIn/LogIn";
 import { useState } from "react";
 
