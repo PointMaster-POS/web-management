fe631a27baa7b33f70327c757027b20156f4f955
Dil0518 <dilrangi20sankalpana@gmail.com>
2024-09-17 03:12:38 +0530

    Create Popups

---
+++ pointmaster/src/components/Popups/PopularItemsModal.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { Modal, Table, Avatar } from "antd";
+import { PopularItemsList } from "../Data";
+
+
+const PopularItemsModal = ({ visible, onClose }) => {
+  const columns = [
+    {
+      title: "Item",
+      dataIndex: "image",
+      key: "image",
+      render: (image) => <Avatar src={image} size={50} />,
+    },
+    {
+      title: "",
+      dataIndex: "name",
+      key: "name",
+    },
+    {
+      title: "Sales",
+      dataIndex: "sales",
+      key: "sales",
+    },
+  ];
+
+  return (
+    <Modal
+      title="Popular Items"
+      visible={visible}
+      onCancel={onClose}
+      footer={null}
+      width={800}
+    >
+      <Table columns={columns} dataSource={PopularItemsList} pagination={{pageSize: 5}} />
+    </Modal>
+  );
+};
+
+export default PopularItemsModal;
