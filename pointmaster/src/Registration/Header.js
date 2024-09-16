6348bf02a67ec566520ecd78515032a0c0438b51
Dil0518 <dilrangi20sankalpana@gmail.com>
2024-09-17 03:04:24 +0530

    Register updates

---
+++ pointmaster/src/Registration/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './header.css'
+
+export default function HeaderReg() {
+  return (
+    <header className="header">
+        <div className="header-title">PointMaster</div>
+    </header>
+  )
+}
