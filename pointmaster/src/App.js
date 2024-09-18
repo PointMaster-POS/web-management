054a2c871090a56087b200c32d65fd8267fa1a63
Dil0518 <dilrangi20sankalpana@gmail.com>
2024-09-18 20:14:42 +0530

    Update App.js

--- pointmaster/src/App.js
+++ pointmaster/src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
+import RegisterNewBusiness from "./pages/RegisterNewBusiness/RegisterNewBusiness";
+import RegisterOwner from "./pages/RegisterOwner/RegisterOwner";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainLayout from "./components/ProtectedRoute/MainLayout";
 import LogIn from "./pages/LogIn/LogIn";
-import Landing from "./pages/Registration/LandingPage/Landing";
+import Landing from "./pages/LandingPage/Landing";
 import Dashboard from "./pages/Dashboard/Dashboard";
 import Stores from "./pages/Stores/Stores";
 import Employees from "./pages/Employees/Employees";
@@ -18,42 +20,45 @@ import { AuthProvider } from "./context/AuthContext";
 
 const App = () => {
   return (
-    <AuthProvider>
-      <BrowserRouter>
-        <Routes>
-          {/* Public Routes */}
-          {/* <Route path="/" element={<Landing />} /> */}
-          <Route path="/login" element={<LogIn />} />
-          <Route path="/forgot-password" element={<LogIn forgotPassword={true} />} />
+    // <RegisterNewBusiness />
+    // <RegisterOwner/>
 
-          {/* Protected Routes */}
-          <Route
-            path="/"
-            element={
-              <ProtectedRoute>
-                <MainLayout />
-              </ProtectedRoute>
-            }
-          >
-            {/* Nested Routes for internal pages */}
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/stores" element={<Stores />} />
-            <Route path="/employees" element={<Employees />} />
-            <Route path="/expired" element={<Expired />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/category" element={<Category />} />
-            <Route path="/suppliers" element={<Suppliers />} />
-            <Route
-              path="/phistory/:supplier_id"
-              element={<PurchaseHistory />}
-            />
-            <Route path="/products" element={<Products />} />
-            <Route path="/orders" element={<Orders />} />
-            <Route path="*" element={<h1>404 Not Found</h1>} /> {/* Catch-all route for unmatched paths */}
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </AuthProvider>
+        <AuthProvider>
+          <BrowserRouter>
+            <Routes>
+              {/* Public Routes */}
+              <Route path="/" element={<Landing />} />
+              <Route path="/login" element={<LogIn />} />
+              <Route path="/forgot-password" element={<LogIn forgotPassword={true} />} />
+
+              {/* Protected Routes */}
+              <Route
+                path="/"
+                element={
+                  <ProtectedRoute>
+                    <MainLayout />
+                  </ProtectedRoute>
+                }
+              >
+                {/* Nested Routes for internal pages */}
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/stores" element={<Stores />} />
+                <Route path="/employees" element={<Employees />} />
+                <Route path="/expired" element={<Expired />} />
+                <Route path="/profile" element={<Profile />} />
+                <Route path="/category" element={<Category />} />
+                <Route path="/suppliers" element={<Suppliers />} />
+                <Route
+                  path="/phistory/:supplier_id"
+                  element={<PurchaseHistory />}
+                />
+                <Route path="/products" element={<Products />} />
+                <Route path="/orders" element={<Orders />} />
+                <Route path="*" element={<h1>404 Not Found</h1>} /> {/* Catch-all route for unmatched paths */}
+              </Route>
+            </Routes>
+          </BrowserRouter>
+        </AuthProvider>
   );
 };
 
