48545cbbe3f686e9e2b34b156aae521b008974a4
Dil0518 <dilrangi20sankalpana@gmail.com>
2024-09-21 02:02:16 +0530

    Create babel.config.js

---
+++ pointmaster/babel.config.js
@@ -0,0 +1,8 @@
+// babel.config.js
+module.exports = {
+    presets: [
+      '@babel/preset-env', // For transforming ES6+ code
+      '@babel/preset-react' // For transforming JSX
+    ],
+  };
+  
