c9a0f4244c732f05af04fba4efae2d3cb6f751ad
Dil0518 <dilrangi20sankalpana@gmail.com>
2024-09-22 22:57:56 +0530

    Update jest.config.js

--- pointmaster/jest.config.js
+++ pointmaster/jest.config.js
@@ -1,17 +1,12 @@
 module.exports = {
-  // Tell Jest to use Babel for transformation
   transform: {
     "^.+\\.jsx?$": "babel-jest",
   },
-  // Ignore transformations in node_modules except for Ant Design
   transformIgnorePatterns: [
-    "node_modules/(?!antd|@ant-design|rc-.+?|@babel/runtime)"
+    "/node_modules/(?!antd|@ant-design|rc-.+?|@babel/runtime|axios)"
   ],
-  // Set up test environment
   testEnvironment: "jsdom",
-  // Resolve .js and .jsx extensions
   moduleFileExtensions: ["js", "jsx"],
-  // Handle CSS imports
   moduleNameMapper: {
     "\\.(css|less)$": "identity-obj-proxy"
   },
