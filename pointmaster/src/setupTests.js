9dec32a27b0ea19d487e44adb20392d90cb964fa
Dil0518 <dilrangi20sankalpana@gmail.com>
2024-09-22 07:48:12 +0530

    Update setupTests.js

--- pointmaster/src/setupTests.js
+++ pointmaster/src/setupTests.js
@@ -1,5 +1,14 @@
 // jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom'; // Custom matchers for Testing Library
+
+// Mock for window.matchMedia
+global.matchMedia = jest.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), // Deprecated
+    removeListener: jest.fn(), // Deprecated
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+}));
