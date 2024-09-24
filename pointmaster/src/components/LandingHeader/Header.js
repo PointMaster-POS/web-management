d91a5a4e4d596677cdb4c0a45fe1e2d048afd36b
Dil0518 <dilrangi20sankalpana@gmail.com>
2024-09-25 01:42:59 +0530

    Update Header.js

--- pointmaster/src/components/LandingHeader/Header.js
+++ pointmaster/src/components/LandingHeader/Header.js
@@ -1,17 +1,30 @@
 // components/Registration/Header.js
-import React from 'react';
-import './header.css';
-import { Button,Typography } from 'antd';
+import React from "react";
+import "./header.css";
+import { Button, Typography } from "antd";
+import { useNavigate } from "react-router-dom";
 
 export default function Header() {
+  const navigate = useNavigate();
+  const handleScrollToContact = () => {
+    const contactSection = document.getElementById("contact-section");
+    if (contactSection) {
+      contactSection.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   return (
     <header className="header">
-      <Typography.Title level={2} style={{color:'white'}}>
+      <Typography.Title level={2} style={{ color: "white" }}>
         Point Master
       </Typography.Title>
       <nav className="nav-links">
-        <a href="#contact">Contact Us</a>
-        <Button type="primary" className="login-btn">
+        <a onClick={handleScrollToContact}>Contact Us</a>
+        <Button
+          type="primary"
+          className="login-btn"
+          onClick={() => navigate("/login")} // Wrap the navigate call in an anonymous function
+        >
           Log In
         </Button>
       </nav>
