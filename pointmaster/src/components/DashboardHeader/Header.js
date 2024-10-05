9d20d54a0c8bda1e704a5f8c20b5d6cd20c5e335
Himindu-Kularathne <himindu.21@cse.mrt.ac.lk>
2024-10-05 14:25:58 +0530

    changes of ip addresses in header

--- pointmaster/src/components/DashboardHeader/Header.js
+++ pointmaster/src/components/DashboardHeader/Header.js
@@ -60,7 +60,7 @@ const Header = ({ setIsAuthenticated }) => {
       return;
     }
     try {
-      const response = await fetch("http://localhost:3001/branch", {
+      const response = await fetch("http://209.97.173.123:3001/branch", {
         method: "GET",
         headers: {
           Authorization: `Bearer ${token}`,
