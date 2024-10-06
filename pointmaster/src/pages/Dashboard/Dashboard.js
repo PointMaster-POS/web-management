3dc5d0336805ed29496dcf506c82af73d3575bc4
Dil0518 <dilrangi20sankalpana@gmail.com>
2024-10-06 19:58:07 +0530

    Update Dashboard.js

--- pointmaster/src/pages/Dashboard/Dashboard.js
+++ pointmaster/src/pages/Dashboard/Dashboard.js
@@ -23,9 +23,10 @@ import PopularItemsModal from "../../components/Popups/PopularItemsModal";
 import OutOfStockModal from "../../components/Popups/OutOfStockModal";
 import { OutOfStockList } from "../../components/Data";
 import { PopularItemsList } from "../../components/Data";
-import { Bar } from "react-chartjs-2";
+import SalesModal from "../../components/Popups/SalesModel/SalesModal";
+import { Bar, Pie } from "react-chartjs-2";
 import { Chart, registerables } from "chart.js";
-import moment from "moment";
+import dayjs from "dayjs";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import "./Dashboard.css";
@@ -70,8 +71,11 @@ const Dashboard = () => {
                 title="Profit"
               />
             </Col>
-            <Col span={24}>
-              <MultiLineChart />
+            <Col span={12}>
+              <BillsBarChart />
+            </Col>
+            <Col span={12}>
+              <BillsPieChart />
             </Col>
           </Row>
         </Col>
@@ -257,13 +261,13 @@ const PaymentMethodCard = ({ icon }) => {
 
   const menu = (
     <Menu onClick={handleMenuClick}>
-      <Menu.Item key="Cash" style={{ fontWeight: "bold", fontSize: "16px", width: "100px" }}>
+      <Menu.Item
+        key="Cash"
+        style={{ fontWeight: "bold", fontSize: "16px", width: "100px" }}
+      >
         Cash
       </Menu.Item>
-      <Menu.Item
-        key="Card"
-        style={{ fontWeight: "bold", fontSize: "16px" }}
-      >
+      <Menu.Item key="Card" style={{ fontWeight: "bold", fontSize: "16px" }}>
         Card
       </Menu.Item>
     </Menu>
@@ -290,7 +294,6 @@ const PaymentMethodCard = ({ icon }) => {
     </Card>
   );
 };
-
 
 const ExpiresCard = ({ icon, title }) => {
   const [expiresCount, setExpiresCount] = useState(0);
@@ -359,10 +362,8 @@ const PopularItems = () => {
   const [loading, setLoading] = useState(false);
 
   // Get today's date and 30 days prior
-  const today = moment().format("YYYY-MM-DD");
-  console.log(today);
-  const thirtyDaysAgo = moment().subtract(30, "days").format("YYYY-MM-DD");
-  console.log(thirtyDaysAgo);
+  const today = dayjs().format("YYYY-MM-DD");
+  const thirtyDaysAgo = dayjs().subtract(30, "days").format("YYYY-MM-DD");
 
   const fetchPopularItems = async (startDate, endDate) => {
     setLoading(true);
@@ -372,7 +373,7 @@ const PopularItems = () => {
       return;
     }
 
-    const adjustedEndDate = moment(endDate).add(1, "days").format("YYYY-MM-DD");
+    const adjustedEndDate = dayjs(endDate).add(1, "days").format("YYYY-MM-DD");
 
     try {
       const response = await fetch(
@@ -440,8 +441,15 @@ const PopularItems = () => {
         renderItem={(item) => (
           <List.Item>
             <List.Item.Meta
-              avatar={<Avatar src={/* item.image_url */ item.image} size={50} />}
-              title={<Text className="item-title"> {/* item.item_name */ item.name} </Text>}
+              avatar={
+                <Avatar src={/* item.image_url */ item.image} size={50} />
+              }
+              title={
+                <Text className="item-title">
+                  {" "}
+                  {/* item.item_name */ item.name}{" "}
+                </Text>
+              }
               description={
                 <Text type="secondary" className="item-description">
                   Sales: {/* item.purchase_count */ item.sales}
@@ -467,7 +475,7 @@ const OutOfStock = () => {
   };
 
   return (
-    <Card >
+    <Card>
       <div className="card-header">
         <Title level={4}>Out of Stock</Title>
         <Text
@@ -495,6 +503,353 @@ const OutOfStock = () => {
   );
 };
 
+const BillsBarChart = () => {
+  const [modalVisible, setModalVisible] = useState(false);
+  const [chartData, setChartData] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-const BillsBarChart = () => {}
+  // Get today's date and 30 days prior
+  const end_month = dayjs().format("YYYY-MM");
+  console.log(end_month);
+  const start_month = dayjs().subtract(12, "month").format("YYYY-MM");
+  console.log(start_month);
+
+  /* const fetchBillsData = async (startMonth, endMonth) => {
+    setLoading(true);
+    const token = localStorage.getItem("accessToken");
+    if (!token) {
+      message.error("Authorization token is missing. Please log in again.");
+      return;
+    }
+
+    // const adjustedEndDate = moment(endDate).add(1, "days").format("YYYY-MM-DD");
+
+    try {
+      const response = await fetch(
+        `http://209.97.173.123:3001/dashboard/business/sale-report/number-of-bills/${startMonth}/${endMonth}`,
+        {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const result = await response.json();
+      console.log("Fetched data: ", result); // Check the fetched data
+
+      if (result.data && result.data.length > 0) {
+        const labels = result.data.map((item) => item.bill_month); // Extract bill_month
+        const values = result.data.map((item) => item.number_of_bills); // Extract number_of_bills
+
+        setChartData({
+          labels,
+          datasets: [
+            {
+              label: "Number of Bills",
+              data: values,
+              backgroundColor: "rgba(75, 192, 192, 0.6)",
+              borderColor: "rgba(75, 192, 192, 1)",
+              borderWidth: 1,
+            },
+          ],
+        });
+      } else {
+        setChartData([]); // Ensure chartData is empty if no data is returned
+      }
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      message.error("Failed to fetch data.");
+    } finally {
+      setLoading(false);
+    }
+  }; */
+
+  // useEffect(() => {
+  //   fetchBillsData(start_month, end_month); // Fetch items on component load
+  // }, []);
+
+  const dummyData = {
+    labels: [
+      "2024-01",
+      "2024-02",
+      "2024-03",
+      "2024-04",
+      "2024-05",
+      "2024-06",
+      "2024-07",
+      "2024-08",
+      "2024-09",
+      "2024-10",
+    ],
+    datasets: [
+      {
+        data: [10, 15, 8, 20, 25, 18, 22, 16, 19, 3],
+        backgroundColor: "#1E3E62",
+        borderColor: "rgba(75, 192, 192, 1)",
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  const handleViewAllClick = () => {
+    setModalVisible(true);
+  };
+
+  const handleCloseModal = () => {
+    setModalVisible(false);
+  };
+
+  const options = {
+    responsive: true,
+    maintainAspectRatio: false,
+    scales: {
+      x: {
+        grid: {
+          display: false,
+        },
+
+        title: {
+          display: true,
+          text: "Months",
+          font: {
+            size: 18
+          }
+        },
+        ticks: {
+          font: {
+            size: 14, // Font size for the x-axis labels
+          },
+        },
+      },
+      y: {
+        grid: {
+          display: false,
+        },
+
+        title: {
+          display: true,
+          text: "Number of Bills",
+          font: {
+            size: 18
+          }
+        },
+        ticks: {
+          font: {
+            size: 14, // Font size for the y-axis labels
+          },
+        },
+        beginAtZero: true,
+      },
+    },
+    plugins: {
+      legend: {
+        display: false, // Hides the legend completely
+      },
+    },
+  };
+
+  return (
+    <Card style={{ height: "516px" }}>
+      <div style={{ display: "flex", justifyContent: "space-between" }}>
+        <Title level={3}>Number of Bills</Title>
+        <Text
+          type="secondary"
+          className="view-more"
+          onClick={handleViewAllClick}
+        >
+          View more...
+        </Text>
+        <SalesModal
+          visible={modalVisible}
+          onClose={handleCloseModal}
+          chartData={dummyData}
+          defaultStartMonth={start_month}
+          defaultEndMonth={end_month}
+          // fetchBillsData={fetchBillsData}
+          options={options}
+        />
+      </div>
+      <div style={{ height: "400px" }}>
+        {loading ? (
+          <p>Loading...</p>
+        ) : dummyData.labels ? (
+          <Bar data={dummyData} options={options} />
+        ) : (
+          <p>No data available</p>
+        )}
+      </div>
+    </Card>
+  );
+};
+
+
+const BillsPieChart = () => {
+  const [modalVisible, setModalVisible] = useState(false);
+  const [chartData, setChartData] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  // Get today's date and 30 days prior
+  //const end_month = moment();
+  //const start_month = moment().subtract(12, "month");
+
+  /* const fetchBillsData = async (startMonth, endMonth) => {
+    setLoading(true);
+    const token = localStorage.getItem("accessToken");
+    if (!token) {
+      message.error("Authorization token is missing. Please log in again.");
+      return;
+    }
+
+    // const adjustedEndDate = moment(endDate).add(1, "days").format("YYYY-MM-DD");
+
+    try {
+      const response = await fetch(
+        `http://209.97.173.123:3001/dashboard/business/sale-report/number-of-bills/${startMonth}/${endMonth}`,
+        {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const result = await response.json();
+      console.log("Fetched data: ", result); // Check the fetched data
+
+      if (result.data && result.data.length > 0) {
+        const labels = result.data.map((item) => item.bill_month); // Extract bill_month
+        const values = result.data.map((item) => item.number_of_bills); // Extract number_of_bills
+
+        setChartData({
+          labels,
+          datasets: [
+            {
+              label: "Number of Bills",
+              data: values,
+              backgroundColor: "rgba(75, 192, 192, 0.6)",
+              borderColor: "rgba(75, 192, 192, 1)",
+              borderWidth: 1,
+            },
+          ],
+        });
+      } else {
+        setChartData([]); // Ensure chartData is empty if no data is returned
+      }
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      message.error("Failed to fetch data.");
+    } finally {
+      setLoading(false);
+    }
+  }; */
+
+  // useEffect(() => {
+  //   fetchBillsData(start_month, end_month); // Fetch items on component load
+  // }, []);
+
+  const dummyData = {
+    labels: [
+      "2024-01",
+      "2024-02",
+      "2024-03",
+      "2024-04",
+      "2024-05",
+      "2024-06",
+      "2024-07",
+      "2024-08",
+      "2024-09",
+      "2024-10",
+    ],
+    datasets: [
+      {
+        label: "Number of Bills",
+        data: [10, 15, 8, 20, 25, 18, 22, 16, 19, 3],
+        backgroundColor: [
+          "#FF6384",
+          "#36A2EB",
+          "#FFCE56",
+          "#4BC0C0",
+          "#9966FF",
+          "#FF9F40",
+          "#FF6384",
+          "#36A2EB",
+          "#FFCE56",
+          "#4BC0C0",
+        ],
+        borderColor: [
+          "#FF6384",
+          "#36A2EB",
+          "#FFCE56",
+          "#4BC0C0",
+          "#9966FF",
+          "#FF9F40",
+          "#FF6384",
+          "#36A2EB",
+          "#FFCE56",
+          "#4BC0C0",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  const handleViewAllClick = () => {
+    setModalVisible(true);
+  };
+
+  const handleCloseModal = () => {
+    setModalVisible(false);
+  };
+
+  const options = {
+    responsive: true,
+    maintainAspectRatio: false,
+  };
+
+  return (
+    <Card style={{ height: "516px" }}>
+      <div style={{ display: "flex", justifyContent: "space-between" }}>
+        <Title level={3}>Number of Bills</Title>
+        <Text
+          type="secondary"
+          className="view-all"
+          onClick={handleViewAllClick}
+        >
+          View more
+        </Text>
+        <SalesModal
+          visible={modalVisible}
+          onClose={handleCloseModal}
+          chartData={chartData}
+          // defaultStartMonth={start_month}
+          // defaultEndMonth={end_month}
+          // fetchBillsData={fetchBillsData}
+        />
+      </div>
+      <div style={{ height: "400px" }}>
+        {loading ? (
+          <p>Loading...</p>
+        ) : dummyData.labels ? (
+          <Pie data={dummyData} options={options} />
+        ) : (
+          <p>No data available</p>
+        )}
+      </div>
+    </Card>
+  );
+};
+
+
+
 export default Dashboard;
