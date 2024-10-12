dd8e274ad1653bdf36534a5c84a7930f9d278639
Himindu-Kularathne <himindu.21@cse.mrt.ac.lk>
2024-10-12 17:25:56 +0530

    chnaged routes

--- pointmaster/src/App.js
+++ pointmaster/src/App.js
@@ -31,7 +31,7 @@ const App = () => {
             <Route path="/forgot-password" element={<LogIn forgotPassword={true} />} />
 
             <Route
-              path="/"
+              path="/protected"
               element={
                 <ProtectedRoute>
                   <MainLayout />
